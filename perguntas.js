3406a50aeff5d682a7d601ae748d9b41726796a2
luizhenriquefg6 <luiz.ferreira.godoy@escola.pr.gov.br>
2025-06-06 18:54:16 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
     'História',
     'Qual foi o motivo razão na qual motivou a Segunda Guerra Mundial?',
-    ' A Segunda Guerra Mundial (1939-1945) foi desencadeada por uma combinação de fatores, incluindo o revanchismo alemão após a Primeira Guerra Mundial, o expansionismo nazifascista e a invasão da Polônia pela Alemanha em 1º de setembro de 1939. '
+    ' O revanchismo alemão após a humilhação e as condições impostas pelo Tratado de Versalhes, que culminou com a ascensão do nazismo e a política expansionista de Hitler. '
 )
 
 criaCartao(
